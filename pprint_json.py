64c4a38a4b2da3cd302e4d9c808a1556dc7494d8
Roman C <myownlab@gmail.com>
2017-12-05 16:24:03 +0000

    change var names and remove pprint()

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,21 @@
 import json
 import sys
-from pprint import pprint
 
 
 def load_data(filepath):
     with open(filepath) as file_handler:
-        shops_list = json.load(file_handler)
-    return shops_list
+        json_content = json.load(file_handler)
+    return json_content
 
 
-def pretty_print_json(shops_list):
-    pprint(shops_list)
+def pretty_print_json(json_content):
+    content = json.dumps(json_content, sort_keys=True, indent=4, ensure_ascii=False)
+    print(content)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.exit("Usage: python3 pprint_json.py <path_to_json>")
     filepath = sys.argv[1]
-    sjson = load_data(filepath)
-    pretty_print_json(sjson)
+    content = load_data(filepath)
+    pretty_print_json(content)
